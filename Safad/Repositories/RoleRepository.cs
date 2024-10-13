cb41bd1c30530e1587fdea60d50c42d209f93f6a
juanes2113 <136110782+juanes2113@users.noreply.github.com>
2024-10-13 09:38:37 -0500

    Add files via upload

---
+++ Safad/Repositories/RoleRepository.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Safad.Interfaces;
+using Safad.Models;
+using Safad.Data;
+
+namespace Safad.Repositories
+{
+    public class RoleRepository : Repository<Role>, IRoleRepository
+    {
+        private readonly SafadDBContext _context;
+        public RoleRepository(SafadDBContext context) : base(context)
+        {
+            _context = context;
+        }
+    }
+}
---
+++ Safad/Repositories/UserRepository.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Safad.Interfaces;
+using Safad.Models;
+using Safad.Data;
+
+namespace Safad.Repositories
+{
+    public class UserRepository : Repository<User>, IUserRepository
+    {
+        private readonly SafadDBContext _context;
+
+        public UserRepository(SafadDBContext context) : base(context)
+        {
+            _context = context;
+        }
+
+        /// <summary>
+        /// Obtiene un usuario por su correo electrónico de la base de datos
+        /// </summary>
+        /// <param name="email">Correo electrónico del usuario a buscar</param>
+        /// <returns>Objeto User si se encuentra, de lo contrario null</returns>
+        public async Task<User> GetUserByEmailAsync(string email)
+        {
+            return await _context.Users.FirstOrDefaultAsync(u => u.UserEmail == email);
+        }
+    }
+}
