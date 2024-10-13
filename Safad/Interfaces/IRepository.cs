71b5f67c34c85c40515d76c02db87e1df6d3b4c8
juanes2113 <136110782+juanes2113@users.noreply.github.com>
2024-10-13 09:33:27 -0500

    Add files via upload

---
+++ Safad/Interfaces/IRepository.cs
@@ -0,0 +1,12 @@
+﻿namespace Safad.Interfaces
+{
+    public interface IRepository<T>
+    {
+        Task<IEnumerable<T>> GetAll();
+        Task<T> GetById(int id);
+        Task Add(T entity);
+        Task Update(T entity);
+        Task Delete(T entity);
+        Task<T> GetSequence(T entity);
+    }
+}
---
+++ Safad/Interfaces/IUserRepository.cs
@@ -0,0 +1,9 @@
+﻿using Safad.Models;
+
+namespace Safad.Interfaces
+{
+    public interface IUserRepository : IRepository<User>
+    {
+        Task<User> GetUserByEmailAsync(string email);
+    }
+}
