fe40e5db491020b773cf9fd7f8ee76069e346a63
juanes2113 <136110782+juanes2113@users.noreply.github.com>
2024-10-13 09:32:07 -0500

    Add files via upload

---
+++ Safad/Data/SafadDBContext.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Safad.Models;
+
+namespace Safad.Data
+{
+    public class SafadDBContext : DbContext
+    {
+        public SafadDBContext(DbContextOptions<SafadDBContext> options) : base(options) // Ctor
+        {
+
+        }
+
+        public DbSet<User> Users { get; set; } // prop
+        public DbSet<Role> Roles { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder) // override OnMod
+        {
+            modelBuilder.Entity<User>(tb =>
+            {
+                tb.HasKey(col => col.UserId);
+                tb.Property(col => col.UserId).IsRequired();
+                tb.Property(col => col.UserEmail).IsRequired().HasMaxLength(50);
+                tb.Property(col => col.Password).IsRequired().HasMaxLength(50);
+                tb.HasOne(u => u.Role)
+                    .WithMany(r => r.Users)
+                    .HasForeignKey(u => u.RoleId)
+                    .OnDelete(DeleteBehavior.Cascade);
+            });
+            modelBuilder.Entity<Role>(tb =>
+            {
+                tb.HasKey(col => col.RoleId);
+                tb.Property(col => col.RoleId).IsRequired();
+                tb.Property(col => col.RoleName).IsRequired().HasMaxLength(25);
+            });
+
+            modelBuilder.Entity<User>().ToTable("Users");
+            modelBuilder.Entity<Role>().ToTable("Roles");
+        }
+    }
+}
