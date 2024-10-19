c25fada3c763289da922e17bc1d9f50711cf4ef1
JuanPabaz <jpgthf@gmail.com>
2024-10-19 13:32:07 -0500

    feat(migrations): add pablo dulces first migration

---
+++ Safad/Migrations/20241019182659_primera-migracion-pablo-dulces.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Safad.Migrations
+{
+    /// <inheritdoc />
+    public partial class primeramigracionpablodulces : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
