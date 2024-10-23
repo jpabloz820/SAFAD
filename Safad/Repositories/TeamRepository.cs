d105a2c328698e707ac11f04eeba69b7191a802e
juanes2113 <136110782+juanes2113@users.noreply.github.com>
2024-10-22 20:03:38 -0500

    repository team y category

---
+++ Safad/Repositories/TeamRepository.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Safad.Interfaces;
+using Safad.Models;
+using Safad.Data;
+
+namespace Safad.Repositories
+{
+    public class TeamRepository : Repository<Team>, ITeamRepository
+    {
+        private readonly SafadDBContext _context;
+
+        public TeamRepository(SafadDBContext context) : base(context)
+        {
+            _context = context;
+        }
+    }
+}
