8e13d453899ad1c17a0f0e9cd222914491b3115e
Juan Pablo Zapata Alvarez <juampa.820@gmail.com>
2024-10-24 22:58:28 -0500

    Seguimiento con los metodos del Athleta

---
+++ Safad/Dtos/UpdateUserAthleteDTO.cs
@@ -0,0 +1,16 @@
+﻿namespace Safad.Dtos
+{
+    public class UpdateUserAthleteDTO
+    {
+        public string NameAthlete { get; set; }
+        public string DniAthlete { get; set; }
+        public string Cellphone { get; set; }
+        public string Address { get; set; }
+        public int Age { get; set; }
+        public float Weight { get; set; }
+        public string? PhotoPath { get; set; }
+        public float Height { get; set; }
+        public string Position { get; set; }
+        
+    }
+}
