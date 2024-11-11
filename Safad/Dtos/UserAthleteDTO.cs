4c860d1a6fce25c5bd6fce24334c11fbdfd798d9
Juan Pablo Zapata Alvarez <juampa.820@gmail.com>
2024-11-11 10:48:28 -0500

    Athlete

--- Safad/Dtos/UserAthleteDTO.cs
+++ Safad/Dtos/UserAthleteDTO.cs
@@ -1,18 +1,17 @@
 ﻿namespace Safad.Dtos
 {
-    public class UserAthleteDTO
+    public class UserAthleteDto
     {
+        public string? PhotoPath { get; set; }
         public string NameAthlete { get; set; }
         public string DniAthlete { get; set; }
         public string Cellphone { get; set; }
-        public string Address { get; set; }
-        public int Age { get; set; }
         public float Weight { get; set; }
-        public string? PhotoPath { get; set; }
         public float Height { get; set; }
         public string Position { get; set; }
-        public string UserEmail { get; set; }
+        public string Address { get; set; }
         public string Password { get; set; }
-
+        public int Age { get; set; }
+        public string UserEmail { get; set; }
     }
 }
